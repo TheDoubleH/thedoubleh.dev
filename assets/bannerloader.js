3a5bdcf00b4a4f966157643bbd4e6bb587c341e4
Henrik Helgesen <henrik@helgesen.consulting>
2024-05-22 18:22:17 -0700

    Enable AnswerlyBanner

---
+++ assets/bannerloader.js
@@ -0,0 +1,5 @@
+const script = document.createElement('script')
+script.src = 'https://fcdn.answerly.io/fn.js'
+script.type = 'text/javascript'
+script.setAttribute('data-companyId', '48a6e79f-060a-4f1c-b574-2052ad02f50e')
+document.body.appendChild(script)
